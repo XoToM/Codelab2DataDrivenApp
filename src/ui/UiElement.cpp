35fd4ae064043f99cc142be90e2e0fba67859234
XoToM <xotom.11.23@gmail.com>
2024-02-11 14:17:07 +0000

    Defined the new base version of UiElement

---
+++ src/ui/UiElement.cpp
@@ -0,0 +1,25 @@
+#include "UiElement.h"
+
+
+bool UiElement::isInBounds(float relX, float relY) {	//	Take coorditanes of a point relative to the top left corner of this element, and determine if the given point is inside this element or not
+	return relX >= 0 && relY >= 0 && relX < this->width && relY < this->height;
+}
+void UiElement::onRender(float x, float y) {
+	for (auto child : this->Children) {	//	Call onRender on all the child elements
+		child->onRender(x + child->xPosition, y + child->yPosition);
+	}
+}
+void UiElement::onUpdate() {
+	for (auto child : this->Children) {	//	Call onUpdate on all the child elements
+		child->onUpdate();
+	}
+}
+
+UiElement::UiElement(float x, float y, float width, float height) {
+	this->width = width;	//	Save the arguments of this constructor as fields of this class.
+	this->height = height;
+	this->xPosition = x;
+	this->yPosition = y;
+
+	this->uniqueId = uniqueIdCounter++;	//	Generates a unique id for this element. I have done the maths, and even if this program created 1000 elements per second (1 every millisecond), the computer running this program will decompose before this counter overflows (time for e-waste to decompose is somewhere between 50 years and 1 million years, and this will take aproximately 584 million years to overflow). This means that we can safely assume this id is going to be unique.
+}
---
+++ src/ui/UiElement.h
@@ -0,0 +1,61 @@
+#pragma once
+#include <memory>
+#include <vector>
+
+class UiElement
+{
+private:
+	static long uniqueIdCounter;
+public:
+	std::vector<std::shared_ptr<UiElement>> Children;	//	Stores child elements of this element
+
+	float xPosition;
+	float yPosition;
+	float width;
+	float height;
+	
+	long uniqueId;
+
+	/// <summary>
+	/// Creates this UiElement at the given position, and with the given size.
+	/// </summary>
+	/// <param name="x">X position relative to this element's parent</param>
+	/// <param name="y">Y position relative to this element's parent</param>
+	/// <param name="width">Width of this element</param>
+	/// <param name="height">Height of this element</param>
+	UiElement(float x, float y, float width, float height);
+
+	/// <summary>
+	/// Renders this element. Base implementation also calls this on this element's children.
+	/// </summary>
+	/// <param name="x">X coordinate of this element relative to the window</param>
+	/// <param name="y">Y coordinate of this element relative to the window</param>
+	virtual void onRender(float x, float y);
+
+	/// <summary>
+	/// Updates this element. Base implementation also calls this on this element's children.
+	/// </summary>
+	virtual void onUpdate();
+
+	/// <summary>
+	/// Takes coorditanes of a point relative to the top left corner of this element, and determines if the given point is inside this element or not
+	/// </summary>
+	/// <param name="relX">X coordinate of the point</param>
+	/// <param name="relY">Y coordinate of the point</param>
+	/// <returns>True if the given point is inside this element, false otherwise.</returns>
+	bool isInBounds(float relX, float relY);
+
+	/// <summary>
+	/// Takes a shared pointer to a UI Element and adds it as a child of this element. Automatically casts the pointer to UIElement if needed.
+	/// </summary>
+	/// <typeparam name="T">Type of the UiElement. Has to extend UiElement.</typeparam>
+	/// <param name="new_child">shared pointer to the child element to be added.</param>
+	/// <returns>The same pointer to the child it was given. Useful for chaining method calls on the child element.</returns>
+	template<class T> std::shared_ptr<T> addChild(std::shared_ptr<T> new_child) {	//	Templates have to be implemented in header files due to how c++ compilers work.
+		static_assert(std::is_base_of_v<UiElement, T>, "Class needs to extend UiElement");		//	Throw a compile time error if the element we are trying to add is not a ui element
+		std::shared_ptr<UiElement> child = std::static_pointer_cast<UiElement, T>(new_child);	//	Casts the shared pointer to a UiElement Shared Pointer
+		this->Children.push_back(child);
+		return new_child;
+	}
+};
+
