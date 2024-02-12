c19d7c4fce0e34e2318d05904a1a42b811217787
XoToM <xotom.11.23@gmail.com>
2024-02-12 01:11:38 +0000

    Pokemon Gen 3 like UI frames

--- src/ui/UiBox.cpp
+++ src/ui/UiBox.cpp
@@ -2,19 +2,53 @@
 #include "ofMain.h"
 
 void UiBox::onRender(float x, float y) {
-	ofSetColor(this->getFillColor());
-	if (this->borderRadius > 0) {
-		ofRectRounded(x, y, this->width, this->height, this->borderRadius);
+
+	if (this->borderWidth > 0) {	//	If we have a border we should render 2 boxes, otherwise we only need 1
+
+		ofSetColor(this->borderColor);	//	Render the outline
+		if (this->borderRadius > 0) {		//	If the box has a radius we should use the more advanced and more computationally expensive rendering function
+			ofRectRounded(x - this->borderWidth / 2, y - this->borderWidth / 2, this->width + this->borderWidth, this->height + this->borderWidth, this->borderRadius);
+		}
+		else {
+			ofDrawRectangle(x - this->borderWidth / 2, y - this->borderWidth / 2, this->width + this->borderWidth, this->height + this->borderWidth);
+		}
+
+		ofSetColor(this->getFillColor());	//	Render the main box
+		if (this->borderRadius > 0) {		//	If the box has a radius we should use the more advanced and more computationally expensive rendering function
+			ofRectRounded(x + this->borderWidth / 2, y + this->borderWidth / 2, this->width - this->borderWidth, this->height - this->borderWidth, this->borderRadius);
+		}
+		else {
+			ofDrawRectangle(x + this->borderWidth / 2, y + this->borderWidth / 2, this->width - this->borderWidth, this->height - this->borderWidth);
+		}
 	}
 	else {
-		ofDrawRectangle(x, y, this->width, this->height);
+		ofSetColor(this->getFillColor());
+		if (this->borderRadius > 0) {		//	If the box has a radius we should use the more advanced and more computationally expensive rendering function
+			ofRectRounded(x, y, this->width, this->height, this->borderRadius);
+		}
+		else {
+			ofDrawRectangle(x, y, this->width, this->height);
+		}
 	}
+	
 
 	UiElement::onRender(x, y);	//	Call onRender on this element's children
 }
-ofColor UiBox::getFillColor() {
-	return this->fillColor;
+
+ofColor UiBox::getFillColor() {	
+	return this->fillColor;	//	Simply return the fill color. Normal boxes don't have any hover and click logic, but allow elements based on them to re-use their rendering mechanism if they only need to change the colours by overriding this implementation.
 }
+
 UiBox::UiBox(float x, float y, float width, float height, ofColor fillColor) : UiElement(x, y, width, height) {
 	this->fillColor = fillColor;
+}
+
+std::shared_ptr<UiBox> UiBox::make_frame(float x, float y, float width, float height) {
+	auto box = make_shared<UiBox>(x,y, width,height);	//	Sets up the frame to look similar to Pokemon Gen 3's border. 
+	box->borderColor = ofColor(0, 0, 128);		//	Make the border blue
+	box->borderRadius = 7.5;
+	box->borderWidth = 7.5;
+
+	box->fillColor = ofColor(255, 255, 255);		//	Make box itself white
+	return box;
 }
--- src/ui/UiBox.h
+++ src/ui/UiBox.h
@@ -6,11 +6,23 @@ class UiBox : public UiElement
 {
 public:
 	ofColor fillColor;
+	ofColor borderColor = ofColor(0,0,0);
 	float borderRadius = 0;
+	float borderWidth = 0;
 
 	UiBox(float x, float y, float width, float height, ofColor fillColor = ofColor(0,0,0));
 	void onRender(float x, float y);
 
 	virtual ofColor getFillColor();
+
+	/// <summary>
+	/// Renders a Pokemon Gen 3 like frame. The actual games had a more complex box than this, but they also rendered their frames through tiles, so they had more flexibility over how each frame looked.
+	/// </summary>
+	/// <param name="x">X coordinate of the box</param>
+	/// <param name="y">Y coordinate of the box</param>
+	/// <param name="width">Width of the box</param>
+	/// <param name="height">Height of the box</param>
+	/// <returns></returns>
+	static std::shared_ptr<UiBox> make_frame(float x, float y, float width, float height);
 };
 
